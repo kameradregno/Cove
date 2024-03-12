ab7413c7beacb0b65b679c36604e21668474b90a
kameradregno <regon0705@gmail.com>
2024-03-12 15:04:27 +0500

    tes

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -1,45 +1,35 @@
 {{-- header --}}
-<nav
-      class="navbar navbar-expand-lg"
-      style="height: 70px; background-color: #3e423c !important"
-    >
-      <div class="container-fluid">
-        <a class="navbar-brand text-light ms-3" href="#"><img src="/frontend/images/content/logo.png" width="60" height="55" alt=""></a>
+<nav class="navbar navbar-expand-lg" style="height: 70px; background-color: #3e423c !important">
+    <div class="container-fluid">
+        <a class="navbar-brand text-light ms-3 mb-1" href="#"><img src="/frontend/images/content/logo.png" width="60"
+                height="55" alt=""></a>
         <div class="d-flex ms-auto">
-          <div
-            class="icon-group d-flex me-3 align-items-center"
-            style="justify-content: flex-end; gap: 2rem"
-          >
-            <a href="#">
-              <i class="bx bx-cart" style="font-size: 24px; color: #a78478"></i>
-            </a>
-            <a href="#">
-              <i
-                class="bx bx-search"
-                style="font-size: 24px; color: #a78478"
-              ></i>
-            </a>
-            <div class="dropdown me-0">
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Sign in</a></li>
-                <li><a class="dropdown-item" href="#">Register</a></li>
-              </ul>
-              <button
-                type="button"
-                class="btn text-light dropdown-toggle rounded-0"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style="
+            <div class="icon-group d-flex me-3 align-items-center" style="justify-content: flex-end; gap: 2rem">
+                <div class="input-group align-items-center rounded-1">
+                    <input type="search" class="form-control me-3 rounded-1" placeholder="Search" aria-label="Search"
+                        aria-describedby="search-addon" />
+                    <i class="bx bx-search" style="font-size: 24px; color: #a78478"></i>
+                </div>
+                <a href="#">
+                  <i class="bx bx-cart" style="font-size: 24px; color: #a78478"></i>
+                </a>
+                <div class="dropdown me-0">
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Sign in</a></li>
+                        <li><a class="dropdown-item" href="#">Register</a></li>
+                    </ul>
+                    <button type="button" class="btn text-light dropdown-toggle rounded-1" data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        style="
                   background-color: #a78478 !important;
                   height: 40px;
-                  font-size: 14.5px;
-                "
-              >
-                Register or Sign in
-              </button>
+                  font-size: 20px;
+                ">
+                        <i class='bx bx-log-in'></i>
+                    </button>
+                </div>
+                <hr style="stroke: #a78478" />
             </div>
-            <hr style="stroke: #a78478" />
-          </div>
         </div>
-      </div>
-    </nav>
+    </div>
+</nav>
