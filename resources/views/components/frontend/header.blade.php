c41bfc7996b8cda745c5da4f3bd666d6f5b4711e
kameradregno <regon0705@gmail.com>
2024-03-20 11:03:06 +0500

    tes

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -18,16 +18,16 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link text-white fw-light" href="#">Beranda</a>
+            <a class="nav-link text-white fw-light" style="font-size: 13px;" href="#">Beranda</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white fw-light" href="#">Dashboard</a>
+            <a class="nav-link text-white fw-light" style="font-size: 13px;" href="#">Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white fw-light" href="#">Customer</a>
+            <a class="nav-link text-white fw-light" style="font-size: 13px;" href="#">Customer</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white fw-light" href="#">Laporan</a>
+            <a class="nav-link text-white fw-light" style="font-size: 13px;" href="#">Laporan</a>
           </li>
         </ul>
 
