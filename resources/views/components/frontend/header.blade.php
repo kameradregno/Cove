20f0af7a3d7417c7323c5cbf67e0dc277eaa000c
kameradregno <regon0705@gmail.com>
2024-03-20 15:59:09 +0500

    tes

--- resources/views/components/frontend/header.blade.php
+++ resources/views/components/frontend/header.blade.php
@@ -24,7 +24,7 @@
             <a class="nav-link text-white fw-light" style="font-size: 14px;" href="/frontend/pages/dashboard.html">Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-white fw-light" style="font-size: 14px;" href="#">Customer</a>
+            <a class="nav-link text-white fw-light" style="font-size: 14px;" href="/frontend/pages/customer.html">Customer</a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-white fw-light" style="font-size: 14px;" href="#">Laporan</a>
