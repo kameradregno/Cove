a7ef5b3484062225359ad992b14b9a85cdbdeb65
kameradregno <regon0705@gmail.com>
2024-03-20 10:04:48 +0500

    tes

---
+++ resources/views/components/frontend/header.blade.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #353535">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        aria-label="Toggle navigation"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        data-mdb-target="#navbarSupportedContent"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link text-white fw-light" href="#">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fw-light" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fw-light" href="#">Customer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fw-light" href="#">Laporan</a>
+          </li>
+        </ul>
+
+        <div
+          class="icon-group d-flex me-3 align-items-center"
+          style="justify-content: flex-end; gap: 0.8rem"
+        >
+          <!-- <a href="#">
+            <i class="bx bxs-user-circle fs-2 text-light ms-4"></i>
+          </a> -->
+          <div class="dropdown">
+            <button
+              class="dropdown-toggle shadow-0 text-light text-end"
+              style="background-color: #353535; border: none; width: 169px"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              User
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Daftar</a></li>
+              <li><a class="dropdown-item" href="#">Masuk</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Container wrapper -->
+  </nav>
---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,77 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Beranda</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="shortcut icon"
+      href="/frontend/images/logo.png"
+      type="image/x-icon"
+    />
+    <link
+      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+
+    @include('components.frontend.header')
+
+    <div class="container mt-4">
+      <h6 class="text-center">Halo, User</h6>
+      <h4 class="text-center fw-light">
+        Aktivitas apa yang ingin anda lakukan hari ini?
+      </h4>
+    </div>
+
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="card border-secondary mx-auto mb-3" style="width: 22rem; height: 167px;">
+          <div class="card-body align-items-center justify-content-center">
+            <h5 class="card-title text-center">Tambah Customer</h5>
+          </div>
+        </div>
+        <div class="card border-secondary mx-auto mb-3" style="width: 22rem; height: 167px;">
+          <div class="card-body align-items-center justify-content-center">
+            <h5 class="card-title text-center">Tambah Pesanan</h5>
+          </div>
+        </div>
+        <div class="card border-secondary mx-auto mb-3" style="width: 22rem; height: 167px;">
+          <div class="card-body align-items-center justify-content-center">
+            <h5 class="card-title text-center">Edit Akun</h5>
+          </div>
+        </div>
+        <div class="card border-secondary mx-2" style="width: 22rem; height: 167px;">
+          <div class="card-body align-items-center justify-content-center">
+            <h5 class="card-title text-center">Pendapatan Bulan Ini</h5>
+          </div>
+        </div>
+        <div class="card border-secondary mx-2" style="width: 22rem; height: 167px;">
+          <div class="card-body align-items-center justify-content-center">
+            <h5 class="card-title text-center">Note Dari Ibu</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+      integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+      integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
