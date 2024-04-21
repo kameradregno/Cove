e191f1ddc1ac473d03dc9392fac7b3cb6323707d
kameradregno <regon0705@gmail.com>
2024-04-21 10:19:49 +0300

    tes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,29 +12,16 @@
     <style>
         body {
             background-color: #f0f0f0;
+            margin-left: 290px;
+            padding: 0px 10px;
         }
     </style>
 </head>
 
 <body>
-
-    <div class="wrapper d-flex align-items-stretch">
-        {{-- sidebar --}}
-        @include('layouts.app.header')
-
-
-
-
-        <div id="content" class="p-4" style="width: 1000px;">
-
-            @yield('content')
-
-        </div>
-
+    <div class="wrapper d-flex align-items-stretch"> {{-- sidebar --}} @include('layouts.app.header') <div
+            id="content" class="p-4" style="width: 1000px;"> @yield('content') </div>
     </div>
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
@@ -46,8 +33,6 @@
     <script src="js/jquery-3.5.1.js"></script>
     <script src="js/jquery.dataTables.min.js"></script>
     <script src="js/script.js"></script>
-    
-
 </body>
 
 </html>
--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -1,48 +1,29 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px; height: 700px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <img src="/frontend/images/logo.png" class="mx-3" width="40" height="40">
-        <use xlink:href="{{ url('/') }}"></use>
-        </img>
+<div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
+    style="width: 280px; height:585px; position: fixed; z-index: 1;overflow-x: hidden; padding: 8px 0;top: 0px; left: 0px; padding: 8px 0;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"> <img
+            src="/frontend/images/logo.png" class="mx-3" width="40" height="40">
+        <use xlink:href="{{ url('/') }}"></use> </img>
     </a>
     <hr />
     <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-            <a href="{{ url('/') }}" class="nav-link text-white" aria-current="page">
-                <i class="bx bx-home text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i>
-                Beranda
-            </a>
+        <li class="nav-item"> <a href="{{ url('/') }}" class="nav-link text-white" aria-current="page"> <i
+                    class="bx bx-home text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i> Beranda </a>
         </li>
-        <li>
-            <a href="{{ url('dashboard') }}" class="nav-link text-white">
-                <i class="bx bxs-dashboard text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i>
-                Dashboard
-            </a>
-        </li>
-        <li>
-            <a href="{{ url('customer') }}" class="nav-link text-white">
-                <i class="bx bx-user text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i>
-                Customer
-            </a>
-        </li>
-        <li>
-            <a href="{{ url('orders') }}" class="nav-link text-white">
-                <i class="bx bxs-receipt text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i>
-                Pesanan
-            </a>
-        </li>
+        <li> <a href="{{ url('dashboard') }}" class="nav-link text-white"> <i
+                    class="bx bx-bar-chart-square text-light mx-2" style="font-size: 20px; vertical-align:-2px"></i>
+                Statisik </a> </li>
+        <li> <a href="{{ url('customer') }}" class="nav-link text-white"> <i class="bx bx-user text-light mx-2"
+                    style="font-size: 20px; vertical-align:-2px"></i> Customer </a> </li>
+        <li> <a href="{{ url('orders') }}" class="nav-link text-white"> <i class="bx bxs-receipt text-light mx-2"
+                    style="font-size: 20px; vertical-align:-2px"></i> Pesanan </a> </li>
     </ul>
     <hr />
-    <div class="dropdown">
-        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-            data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32"
-                class="rounded-circle me-2" />
-            <strong>mdo</strong>
-        </a>
+    <div class="dropdown"> <a href="#"
+            class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown"
+            aria-expanded="false"> <img src="https://github.com/mdo.png" alt="" width="32" height="32"
+                class="rounded-circle me-2" /> <strong>mdo</strong> </a>
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-            <li>
-                <a class="dropdown-item" href="#">New project...</a>
-            </li>
+            <li> <a class="dropdown-item" href="#">New project...</a> </li>
             <li><a class="dropdown-item" href="#">Settings</a></li>
             <li><a class="dropdown-item" href="#">Profile</a></li>
             <li>
