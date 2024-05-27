ecff9ca16776174b52ab1a38879d304394ffb981
kameradregno <regon0705@gmail.com>
2024-05-27 15:14:11 +0300

    tes

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -86,7 +86,7 @@
 
                         <div class="relative">
 
-                            <input type="text" id="table-search"
+                            <input type="text" name="searchcustomer" id="searchcustomer"
                                 class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Cari customer">
 
