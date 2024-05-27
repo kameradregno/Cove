b3acc9aa3c52391022cfa0fd276940568370a308
kameradregno <regon0705@gmail.com>
2024-05-27 15:09:38 +0300

    tes

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -141,43 +141,52 @@
                                 <td class="px-6 py-4">
                                     <a href="{{ url("customer/$customer->id/edit") }}"><button
                                             class="text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button></a>
-                                    <button type="button" data-bs-target="#staticBackdropCustomer"
+                                    <button type="button" data-modal-target="modal-customer"
+                                        data-modal-toggle="modal-customer"
                                         class="text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete</button>
                                 </td>
                             </tr>
 
                             <!-- Modal -->
-                        <div class="modal fade" id="staticBackdropCustomer" data-bs-backdrop="static"
-                        data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
-                        aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
-                                        <i class="bx bxs-error text-danger me-2 " style="font-size: 25px"></i>Perhatian
-                                    </h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    Anda yakin ingin menghapus data customer ini?
-                                </div>
-                                <div class="modal-footer">
-                                    <form action="{{ url("customer/$customer->id") }}" method="POST">
-                                        @method('delete')
-                                        @csrf
-                                        <button type="submit" class="btn btn-outline-danger" data-bs-dismiss="modal">
-                                            Ya
+                            <div id="modal-customer" tabindex="-1"
+                                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                <div class="relative p-4 w-full max-w-md max-h-full">
+                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                        <button type="button"
+                                            class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                            data-modal-hide="popup-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                            <span class="sr-only">Tutup</span>
                                         </button>
-                                    </form>
-                                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                        Tidak
-                                    </button>
+                                        <div class="p-4 md:p-5 text-center">
+                                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 20 20">
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                    stroke-linejoin="round" stroke-width="2"
+                                                    d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                            </svg>
+                                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Apakah
+                                                anda yakin ingin menghapus data ini?</h3>
+                                            <form action="{{ url("customer/$customer->id") }}" method="POST">
+                                                @method('delete')
+                                                @csrf
+                                                <button data-modal-hide="popup-modal" type="submit"
+                                                    class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                                    Ya
+                                                </button>
+                                            </form>
+                                            <button data-modal-hide="popup-modal" type="button"
+                                                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Tidak</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    
-
                         @endforeach
                     </tbody>
                 </table>
