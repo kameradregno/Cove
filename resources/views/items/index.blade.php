b30b73bb65fca734f4260b33be2c73d15e579562
kameradregno <regon0705@gmail.com>
2024-05-28 10:20:11 +0300

    tes

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -142,15 +142,15 @@
                                     <a href="{{ url("items/show/$item->id") }}"><button
                                             class="text-white bg-gray-500 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Detail</button></a>
 
-                                    <button type="button" data-modal-target="modal-item{{ $item->id }}"
-                                        data-modal-toggle="modal-item"
+                                    <button type="button" data-modal-target="{{ $item->id }}"
+                                        data-modal-toggle="{{ $item->id }}"
                                         class="text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete</button>
 
                                 </td>
                             </tr>
 
                             <!-- Modal -->
-                            <div id="modal-item{{ $item->id }}" tabindex="-1"
+                            <div id="{{ $item->id }}" tabindex="-1"
                                 class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                 <div class="relative p-4 w-full max-w-md max-h-full">
                                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
