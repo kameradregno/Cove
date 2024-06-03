d1d8de854d279ba986775aa7c2cdd712ea87e2e5
kameradregno <regon0705@gmail.com>
2024-06-03 14:48:44 +0300

    tes

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -40,7 +40,7 @@
                             <label for="order-name"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Harga
                                 Sprei</label>
-                            <input type="text" name="harga_sprei" id="harga_sprei"
+                            <input type="number" name="harga_sprei" id="harga_sprei"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required />
                         </div>
