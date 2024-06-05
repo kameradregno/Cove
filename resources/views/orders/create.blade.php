e6cc2f97f8d64936549151bf1fd675c8769855f1
FarrelAhmad <farrel.aurell@gmail.com>
2024-06-06 02:57:33 +0700

    tmbh select2select

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -6,6 +6,16 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.js-example-basic-single').select2();
+        });
+    </script>
+
     <div class="p-4 sm:ml-64">
 
         <div class="mt-14">
@@ -23,11 +33,8 @@
                     <form class="w-full" method="POST" action="{{ url('orders') }}">
                         @csrf
                         <div class="mb-5">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama
-                                Pembeli</label>
-                            <select name="customer_id" id="nama_pembeli"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required>
+                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Pembeli</label>
+                            <select name="customer_id" id="nama_pembeli" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 js-example-basic-single">
                                 @foreach ($customers as $customer)
                                     <option value="{{ $customer['id'] }}">{{ $customer['nama'] }}</option>
                                 @endforeach
@@ -85,7 +92,5 @@
                 </div>
 
             </div>
-
         </div>
-
     @endsection
