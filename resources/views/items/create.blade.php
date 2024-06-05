d7bbba857bb60ef7551d63b493291d173dd4cdab
FarrelAhmad <farrel.aurell@gmail.com>
2024-06-05 21:42:17 +0700

    tes

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Customer')
+@section('title', 'Barang Pesanan')
 
 @section('content')
 
--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Customer')
+@section('title', 'Pesanan')
 
 @section('content')
 
