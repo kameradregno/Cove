f05c6afdb28e1f79c707511341008b354610205d
FarrelAhmad <farrel.aurell@gmail.com>
2024-06-27 09:23:24 +0700

    ralat

--- resources/views/auth/Login.blade.php
+++ resources/views/auth/Login.blade.php
@@ -10,7 +10,7 @@
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1
                     class="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign In
+                    Masuk
                 </h1>
 
                 @if (session()->has('error_message'))
@@ -40,7 +40,7 @@
                     </div>
                     <div>
                         <label for="password"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kata Sandi</label>
                         <input type="password" name="password" id="password"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required="">
@@ -48,13 +48,12 @@
 
                     <div class="flex items-center justify-between">
                         <a href=" {{ url('forgetpassword') }} "
-                            class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                            password?</a>
+                            class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Lupa
+                            Kata Sandi?</a>
                     </div>
 
                     <button type="submit"
-                        class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign
-                        in</button>
+                        class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
                         
                 </form>
 
--- resources/views/forgetpassword.blade.php
+++ resources/views/forgetpassword.blade.php
@@ -12,7 +12,7 @@
 
                 <h1
                     class="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Forgot your password?
+                    Lupa Kata Sandi?
                 </h1>
 
                 @if (session()->has('success'))
@@ -36,20 +36,18 @@
 
                     <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Your Email</label>
+                            Email Anda</label>
                         <input type="email" name="email" id="email"
                             class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required="">
                     </div>
 
                     <button type="submit"
-                        class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send
-                        code</button>
+                        class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Kirim Kode</button>
 
                     <p class="text-sm font-light text-center text-gray-500 dark:text-gray-400">
-                        Do you remember the password again? <a href="{{ url('login') }}"
-                            class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-                            in</a>
+                        Sudah ingat kata sandinya? <a href="{{ url('login') }}"
+                            class="font-medium text-primary-600 hover:underline dark:text-primary-500">Masuk</a>
                     </p>
                 </form>
 
